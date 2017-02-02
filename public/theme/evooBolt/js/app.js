f5574f300175a560bab63ca41397ae0fb50b3833
Edbert Voo <developer@edbertvoo.com>
2017-02-01 19:27:42 -0700

    Pace Load Bar Added
    Added pace loading bar

--- public/theme/evooBolt/js/app.js
+++ public/theme/evooBolt/js/app.js
@@ -1,5 +1,15 @@
 $(document).foundation();
 
+//Animate fragments
 $(window).load(function() {
     $('body').fragmentScroll();
+});
+
+//Unhide the body once page has finished loading
+$(function () {
+  Pace.on("done", function () {
+      $("#mainContent").css("visibility", "visible");
+      $("#primary-menu").css("visibility", "visible");
+      $("#footerContainer").css("visibility", "visible");
+  });
 });
--- public/theme/evooBolt/js/min/app-min.js
+++ public/theme/evooBolt/js/min/app-min.js
@@ -1,1 +1,1 @@
-$(document).foundation(),$(window).load(function(){$("body").fragmentScroll()});
+$(document).foundation(),$(window).load(function(){$("body").fragmentScroll()}),$(function(){Pace.on("done",function(){$("#mainContent").css("visibility","visible"),$("#primary-menu").css("visibility","visible"),$("#footerContainer").css("visibility","visible")})});
