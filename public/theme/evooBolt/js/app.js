5d03bc875379f3e5bc18b187ad19ef228bdc418b
Edbert Voo <developer@edbertvoo.com>
2017-02-02 01:26:00 -0700

    Slick Implemented With Responsive Buttons

--- public/theme/evooBolt/js/app.js
+++ public/theme/evooBolt/js/app.js
@@ -1,14 +1,15 @@
 $(document).foundation();
 
 //Animate fragments
-$(window).load(function() {
+$(window).load(function () {
     $('body').fragmentScroll();
 });
 
 //Unhide the body once page has finished loading
 $(function () {
-  Pace.on("done", function () {
-      $(".hiddenLoading").css("visibility", "visible");
+    Pace.on("done", function () {
+        $(".hiddenLoading").css("visibility", "visible");
 
-  });
+    });
 });
+
--- public/theme/evooBolt/js/index.js
+++ public/theme/evooBolt/js/index.js
@@ -31,3 +31,45 @@ if(getMobileOperatingSystem()=='IOS'){
         return "unknown";
     }
 });
+
+
+//slick
+$(document).ready(function () {
+    $('#fpSlick').slick({
+        autoplay:true,
+        mobileFirst: true,
+        prevArrow: "#fpSlickPrev",
+        nextArrow: "#fpSlickNext"
+    });
+
+    calcFpSlickHeight();
+
+    $(window).on("resize", $.throttle(250, function () {
+        calcFpSlickHeight();
+    }));
+
+
+
+    function calcFpSlickHeight() {
+        //Set up arrows
+        var fpSlickHeight=$("#fpSlick").height();
+        var fpSlickWidth=$("#fpSlick").width();
+        var fpSlickOffset=$("#fpSlick").offset().top;
+        var prevArrow=$("#fpSlickPrev");
+        var nextArrow = $("#fpSlickNext");
+
+        //(fpSlickHeight/2)-(prevArrow.height()/2)+
+        //Math.round(fpSlickOffset)
+
+        prevArrow.css("top", (fpSlickHeight/2)+prevArrow.height()/2+fpSlickOffset);
+        nextArrow.css("top", (fpSlickHeight/2)+nextArrow.height()/2+fpSlickOffset);
+        nextArrow.css("left", fpSlickWidth-nextArrow.width());
+
+
+
+        //Set up Text
+        $(".fpWorkTextContainer").css("top", (fpSlickHeight/2)-($(".fpWorkTextContainer").height()/2));
+
+    }
+
+});
